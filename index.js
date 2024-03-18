5eb438eb41f7ff73334a79fea30453d761f956e0
Marcel-Thielbar <160449367+Marcel-Thielbar@users.noreply.github.com>
2024-03-18 14:37:40 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const db = new Dexie('GutDiverse');
 db.version(1).stores({
-    items: '++id, name, isEaten, quantity' // Add quantity field to the table schema
+    items: '++id, name, isEaten, quantity'
 });
 
 const itemForm = document.getElementById('itemForm');
@@ -10,36 +10,31 @@ const totalDiversityScoreDiv = document.getElementById('totalDiversityScoreDiv')
 const populateItemsDiv = async () => {
     const allItems = await db.items.reverse().toArray();
 
-    itemsDiv.innerHTML = allItems.map(item => `
-        <div class="item ${item.isEaten && 'eaten'}">
-            <label>
-                <input type="checkbox" 
-                class="checkbox"
-                onchange="toggleItemStatus(event, ${item.id})"
-                ${item.isEaten && 'checked'}
-                >
-            </label>
+    const checkedItems = allItems.filter(item => item.isEaten);
 
-            <div class="itemInfo">
-                <p>${item.name}</p>
-                <p>${item.quantity}</p>
-            </div>
-            <button class="deleteButton"
-            onclick="removeItem(${item.id})">
-            X
-            </button>
-        </div>
+    itemsDiv.innerHTML = allItems.map(item => `
+    <div class="item ${item.isEaten && 'eaten'}">
+    <label class="checkbox-label">
+        <input type="checkbox" class="checkbox" onchange="toggleItemStatus(event, ${item.id})" ${item.isEaten && 'checked'}>
+        <span class="custom-checkbox"></span>
+    </label>
+
+    <div class="itemInfo">
+        <p>${item.name}</p>
+        <p>${item.quantity}</p>
+    </div>
+    <button class="deleteButton" onclick="removeItem(${item.id})">X</button>
+</div>
     `).join("");
 
-    const arrayOfQuantities = allItems.map(item => Number(item.quantity)); // Convert quantities to numbers
+    const arrayOfQuantities = allItems.map(item => Number(item.quantity));
     const totalQuantity = arrayOfQuantities.reduce((a, b) => a + b, 0);
 
     const totalItems = allItems.length;
-    itemsDiv.innerHTML += `<p>Total Items: ${totalItems}</p>`;
-    totalDiversityScoreDiv.innerText = 'Total Diversity Score: ' + totalItems;
+    
+    itemsDiv.innerHTML += `<p>Total Items on list: ${totalItems}</p>`;
+    totalDiversityScoreDiv.innerText = 'Total Diversity Score: ' + checkedItems.length;
 }
-
-
 
 window.onload = async () => {
     await populateItemsDiv();
@@ -51,8 +46,7 @@ itemForm.onsubmit = async (event) => {
     const name = document.getElementById("nameInput").value;
     const quantity = document.getElementById("quantityInput").value;
 
-    // Assuming isEaten is optional, you can set it to false by default
-    await db.items.add({ name, quantity, isEaten: false }); 
+    await db.items.add({ name, quantity, isEaten: false });
 
     itemForm.reset();
     await populateItemsDiv();
@@ -66,5 +60,5 @@ const toggleItemStatus = async (event, id) => {
 const removeItem = async (id) =>{
     await db.items.delete(id);
     await populateItemsDiv();
-}
+};
 
